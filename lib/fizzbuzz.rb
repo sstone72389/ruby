cf81ce096772dfb62ffe8125cc05ff8295828967
“sstone72389” <sstone72389@gmail.com>
2017-04-18 12:40:25 -0400

    Completes training/fizzbuzz project
    completes required training.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
-
 require 'pry'
 
-# Let `max_num` be the number that you want to count to.
+def fzbz(num)
+  fb = {
+    'fizz' => [],
+    'buzz' => [],
+    'fizzbuzz' => [],
+    'other' => []
+  }
+  1.upto(num) do |i|
+    if (i % 5).zero? && (i % 3).zero?
+      fb['fizzbuzz'] << i
+    elsif (i % 5).zero?
+      fb['buzz'] << i
+    elsif (i % 3).zero?
+      fb['fizz'] << i
+    else
+      fb['other'] << i
+    end
+  end
+  puts fb
+end
+
+binding.pry
+''
